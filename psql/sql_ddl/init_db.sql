653b9040f8caa7781f0d814254d90981ac455dc1
shyleshragun <shylesh.ragunayakam.jrvs.ca@gmail.com>
2019-08-11 16:44:48 +0000

    debug error for port connection error

--- psql/sql_ddl/init_db.sql
+++ psql/sql_ddl/init_db.sql
@@ -1,6 +1,6 @@
-create DATABASE jrvstrading;
-grant all privileges on DATABASE jrvstrading TO postgres;
+CREATE DATABASE jrvstrading;
+GRANT ALL PRIVILEGES ON DATABASE jrvstrading TO postgres;
 
 --db for test case
-create DATABASE jrvstrading_test;
-grant all privileges on DATABASE jrvstrading_test TO postgres;
+CREATE DATABASE jrvstrading_test;
+GRANT ALL PRIVILEGES ON DATABASE jrvstrading_test TO postgres;
